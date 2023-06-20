513fbc1b35c2c219fa46917d51caf9b786545808
kemokemo <t2wonderland@gmail.com>
2023-06-20 18:25:18 +0900

    feat: add targets

--- main.go
+++ main.go
@@ -23,33 +23,35 @@ func run(args []string) int {
 	wg := &sync.WaitGroup{}
 	for _, root := range args[1:] {
 		wg.Add(1)
-		go walk(root, wg, os.Stdout)
+		go walk(root, wg, os.Stdout, []string{"名前", "What"})
 	}
 	wg.Wait()
 
 	return 0
 }
 
-func walk(root string, wg *sync.WaitGroup, w io.Writer) {
+func walk(root string, wg *sync.WaitGroup, w io.Writer, targets []string) {
 	defer wg.Done()
 
-	// Current implementation doesn't seem to generate error.
-	// So, ignore returned error.
 	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
 			return nil
 		}
-		fmt.Fprintf(w, "path: %v\n", path)
+
 		b, err := os.ReadFile(path)
 		if err != nil {
 			return fmt.Errorf("failed to read file: %v, %v", path, err)
 		}
-		i := strings.Index(string(b), "名前")
-		if i != -1 {
-			fmt.Fprintf(w, "%v: %v\n", path, i)
+
+		for _, t := range targets {
+			if strings.Contains(string(b), t) {
+				fmt.Fprintf(w, "%v: %v\n", path, t)
+			}
 		}
+
 		return nil
 	})
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to walk files, %v", err)
 	}
